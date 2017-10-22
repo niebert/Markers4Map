76697c139b90dc9344b701737570e4e388f55990
Engelbert Niehaus <niehaus@uni-landau.de>
2017-10-22 22:28:36 +0200

    Initial Commit for Markers4Map with OpenLayers support

---
+++ docs/db/data.js
@@ -0,0 +1,20 @@
+vDataJSON["initdata"] = [
+    {
+        "id": "2821",
+        "lat": "51.493889053694915",
+        "lng": "-0.14467470703124907",
+        "title": "London",
+        "summary": "London created a Open Innovation Ecosystem for SDG Clean Water and Sanitation",
+        "has_detail_page": "1",
+        "webpage": "https://en.wikipedia.org/wiki/London",
+    },
+    {
+        "id": "1508460902844",
+        "lat": "51.243141",
+        "lng": "-1.81185",
+        "title": "Brimingham",
+        "summary": "Birmingham created SDG-activities about SDG7 Clean and affordable Energy and SDG3 Health",
+        "has_detail_page": "1",
+        "webpage": "https://en.wikipedia.org/wiki/Birmingham",
+    }
+]
---
+++ docs/js/submit2map.js
@@ -0,0 +1,63 @@
+/**
+* Extend object 'a' with the properties of object 'b'.
+* If there's a conflict, content of object 'b' overwrites content of 'a'
+*/
+
+function el4id(pID) {
+  return document.getElementById(pID);
+}
+
+function copyMapCenterZoom() {
+  el4id("viewmapcenter").value = el4id("mymapcenter").value
+  el4id("viewzoom").value = el4id("myzoom").value
+}
+
+function populateDataJSON() {
+  vEditor4JSON.saveLS(); // save JSON Data to Local Storage
+  //copyMapCenterZoom();
+  var vData = vEditor4JSON.aData;
+  var vOut = "";
+  var vCR = "";
+  var vRecOut = "";
+  var vMapCenterMissing = true;
+  for (var i = 0; i < vData.length; i++) {
+    vRecOut = getMarkerString(i+1,vData[i]);
+    if (vRecOut != "") {
+      vOut += vCR + vRecOut;
+      vCR = ",\n";
+      if (vMapCenterMissing) {
+        el4id("mymapcenter").value = vData[i].geolocation;
+        vMapCenterMissing = false;
+      }
+    };
+  };
+  vOut = "[" + vOut + "]";
+  //var vJSON = vData;
+  //document.getElementById('jsondata').value = JSON.stringify(vJSON,null,4);
+  document.getElementById('jsondata').value = vOut;
+};
+
+function getMarkerString(pNr,pRec) {
+  var vOut = "";
+  if ((pRec.lat != "") && (pRec.lng != "")) {
+    vOut += "{ \"geolocation\" : ["+pRec.lat+","+pRec.lng+"],";
+    vOut += "\"name\" : \"";
+    vOut +="<b>";
+    if (pRec.webpage != "") {
+      vOut += "<a href=\""+pRec.webpage+"\" target=\"_blank\">"+pRec.title+"</a>";
+    } else {
+      vOut += pRec.title;
+    };
+    vOut +="</b><br/>";
+    if (pRec.hasOwnProperty("summary")) {
+      if (pRec.summary != "") {
+        vOut += pRec.summary.replace(/"/g,'\"');
+        vOut +="<br/>";
+      }
+    }
+  } else {
+    //console.log("("+pNr+") missing geolocation in record");
+    alert("("+pNr+") missing geolocation in record");
+  };
+  return vOut;
+}
