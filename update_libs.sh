30c330884fbfb81d42079e8eab7aef069e425230
Engelbert Niehaus <niehaus@uni-landau.de>
2018-11-11 07:19:07 +0100

    landau_markers4map.json

---
+++ update_libs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# wget https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js -O docs/js/filesaver.js
+# wget https://raw.githubusercontent.com/eligrey/Blob.js/master/Blob.js -O docs/js/blob.js
+wget https://cdn.rawgit.com/eligrey/Blob.js/0cef2746414269b16834878a8abc52eb9d53e6bd/Blob.js -O docs/js/blob.js
+wget https://cdn.rawgit.com/eligrey/canvas-toBlob.js/f1a01896135ab378aa5c0118eadd81da55e698d8/canvas-toBlob.js  -O docs/js/canvas2blob.js
+wget https://cdn.rawgit.com/eligrey/FileSaver.js/5ed507ef8aa53d8ecfea96d96bc7214cd2476fd2/FileSaver.min.js -O docs/js/filesaver.js
